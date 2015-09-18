7e18b870277a6af45a33fb1111a1cc9268ee7ff8
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-09-18 09:41:58 -0600

    max completed

--- arrays.rb
+++ arrays.rb
@@ -3,5 +3,15 @@ module Arrays
 	def middles(array1, array2)
 		[array1[1], array2[1]]
 	end
+
+	def max(array)
+		max = 0
+		array.each do |element|
+			if element > max 
+				max = element
+			end
+		end
+		max
+	end	
 	
 end
